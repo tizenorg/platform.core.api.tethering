044c9e8c188d69e6ad585a11d8753cdfdb1bfe47
Jinkun Jang <jinkun.jang@samsung.com>
2013-03-13 01:46:16 +0900

    Tizen 2.1 base

---
+++ packaging/capi-network-tethering.spec
@@ -0,0 +1,84 @@
+Name:       capi-network-tethering
+Summary:    Tethering Framework
+Version:    0.0.11
+Release:    1
+Group:      TO_BE/FILLED_IN
+License:    Apache License Version 2.0
+Source0:    %{name}-%{version}.tar.gz
+Requires(post):   /sbin/ldconfig
+Requires(postun): /sbin/ldconfig
+
+BuildRequires: pkgconfig(dlog)
+BuildRequires: pkgconfig(dbus-glib-1)
+BuildRequires: pkgconfig(capi-base-common)
+BuildRequires: pkgconfig(glib-2.0)
+BuildRequires: pkgconfig(vconf)
+BuildRequires: cmake
+
+%description
+Tethering framework library for CAPI
+
+%package devel
+Summary:	Development package for Tethering framework library
+Group:		Development/Libraries
+Requires:	%{name} = %{version}-%{release}
+%description devel
+Development package for Tethering framework library
+
+%prep
+%setup -q
+
+%build
+cmake . -DCMAKE_INSTALL_PREFIX=/usr
+
+make %{?jobs:-j%jobs}
+
+%install
+rm -rf %{buildroot}
+%make_install
+
+%post -p /sbin/ldconfig
+
+%postun -p /sbin/ldconfig
+
+%files
+%manifest capi-network-tethering.manifest
+%defattr(-,root,root,-)
+%{_libdir}/*.so.*
+
+%files devel
+%defattr(-,root,root,-)
+%{_includedir}/network/*.h
+%{_libdir}/pkgconfig/*.pc
+%{_libdir}/*.so
+
+%changelog
+* Thu Jan 24 2013 Seungyoun Ju <sy39.ju@samsung.com> 0.0.11-1
+- Indications for Wi-Fi tethering setting change are added
+- Dbus service / interface / object names are changed
+
+* Tue Jan 15 2013 Seungyoun Ju <sy39.ju@samsung.com> 0.0.10-1
+- Wi-Fi tethering state is not checked when its settings are modified
+
+* Fri Nov 02 2012 Seungyoun Ju <sy39.ju@samsung.com> 0.0.9-1
+- Manifest file is added for SMACK
+
+* Mon Aug 20 2012 Seungyoun Ju <sy39.ju@samsung.com> 0.0.8-1
+- Deprecated APIs are removed
+
+* Wed Aug 01 2012 Seungyoun Ju <sy39.ju@samsung.com> 0.0.7-1
+- Managed APIs are implemented for Wi-Fi tethering settings
+
+* Sat Jul 21 2012 Seungyoun Ju <sy39.ju@samsung.com> 0.0.6-1
+- Fix tethering callback issue (JIRA S1-6197)
+
+* Tue Jul 10 2012 Seungyoun Ju <sy39.ju@samsung.com> 0.0.5
+- Getting MAC address API is implemented
+- TETHERING_TYPE_ALL case is implemented
+- Test code is implemented
+
+* Tue Jun 26 2012 Seungyoun Ju <sy39.ju@samsung.com> 0.0.4
+- All internal APIs are implemented
+
+* Fri Jun 15 2012 Seungyoun Ju <sy39.ju@samsung.com> 0.0.3
+- Deprecated API from Glib2-2.32.3 is replaced with new one
---
+++ src/tethering_client.c
@@ -0,0 +1,119 @@
+/*
+* Copyright (c) 2011 Samsung Electronics Co., Ltd All Rights Reserved
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#include <stdlib.h>
+#include <string.h>
+#include "tethering_private.h"
+
+int tethering_client_clone(tethering_client_h *dest, tethering_client_h origin)
+{
+	_retvm_if(dest == NULL, TETHERING_ERROR_INVALID_PARAMETER,
+			"Parameter(dest) is NULL\n");
+
+	__tethering_client_h *si = NULL;
+
+	si = malloc(sizeof(__tethering_client_h));
+	if (si == NULL) {
+		ERR("malloc is failed\n");
+		return TETHERING_ERROR_OUT_OF_MEMORY;
+	}
+
+	memcpy(si, (__tethering_client_h *)origin,
+			sizeof(__tethering_client_h));
+
+	*dest = (tethering_client_h)si;
+
+	return TETHERING_ERROR_NONE;
+}
+
+int tethering_client_destroy(tethering_client_h client)
+{
+	_retvm_if(client == NULL, TETHERING_ERROR_INVALID_PARAMETER,
+			"Parameter(client) is NULL\n");
+
+	free(client);
+
+	return TETHERING_ERROR_NONE;
+}
+
+int tethering_client_get_tethering_type(tethering_client_h client, tethering_type_e *type)
+{
+	_retvm_if(client == NULL, TETHERING_ERROR_INVALID_PARAMETER,
+			"Parameter(client) is NULL\n");
+	_retvm_if(type == NULL, TETHERING_ERROR_INVALID_PARAMETER,
+			"Parameter(type) is NULL\n");
+
+	__tethering_client_h *si = (__tethering_client_h *)client;
+
+	*type = si->interface;
+
+	return TETHERING_ERROR_NONE;
+}
+
+int tethering_client_get_name(tethering_client_h client, char **name)
+{
+	_retvm_if(client == NULL, TETHERING_ERROR_INVALID_PARAMETER,
+			"Parameter(client) is NULL\n");
+	_retvm_if(name == NULL, TETHERING_ERROR_INVALID_PARAMETER,
+			"Parameter(name) is NULL\n");
+
+	__tethering_client_h *si = (__tethering_client_h *)client;
+
+	*name = strdup(si->hostname);
+	if (*name == NULL) {
+		ERR("strdup is failed\n");
+		return TETHERING_ERROR_OUT_OF_MEMORY;
+	}
+
+	return TETHERING_ERROR_NONE;
+}
+
+int tethering_client_get_ip_address(tethering_client_h client, tethering_address_family_e address_family, char **ip_address)
+{
+	_retvm_if(client == NULL, TETHERING_ERROR_INVALID_PARAMETER,
+			"Parameter(client) is NULL\n");
+	_retvm_if(ip_address == NULL, TETHERING_ERROR_INVALID_PARAMETER,
+			"Parameter(ip_address) is NULL\n");
+
+	__tethering_client_h *si = (__tethering_client_h *)client;
+
+	*ip_address = strdup(si->ip);
+	if (*ip_address == NULL) {
+		ERR("strdup is failed\n");
+		return TETHERING_ERROR_OUT_OF_MEMORY;
+	}
+
+	return TETHERING_ERROR_NONE;
+}
+
+int tethering_client_get_mac_address(tethering_client_h client, char **mac_address)
+{
+	_retvm_if(client == NULL, TETHERING_ERROR_INVALID_PARAMETER,
+			"Parameter(client) is NULL\n");
+	_retvm_if(mac_address == NULL, TETHERING_ERROR_INVALID_PARAMETER,
+			"Parameter(mac_address) is NULL\n");
+
+	__tethering_client_h *si = (__tethering_client_h *)client;
+
+	*mac_address = strdup(si->mac);
+	if (*mac_address == NULL) {
+		ERR("strdup is failed\n");
+		return TETHERING_ERROR_OUT_OF_MEMORY;
+	}
+
+	return TETHERING_ERROR_NONE;
+}
+
